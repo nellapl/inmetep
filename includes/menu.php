039c7486c31f339c5ee55770b6ad497f18744dd3
Nell peña <nellapl2006@gmail.com>
2021-08-08 18:24:13 -0400

    Add files via upload

---
+++ includes/menu.php
@@ -0,0 +1,18 @@
+<div class="card my-3">
+	<div class="card-body">
+		<ul class="nav">
+			<li class="nav-item">
+				<a class="nav-link active" aria-current="page" href="lista_proyectos.php">Proyectos</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="htf.php">Hojas Tecnicas</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" href="lista_opi.php">Ordenes de produccion</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+			</li>
+		</ul>
+	</div>
+</div>
---
+++ includes/scripts.php
@@ -0,0 +1,7 @@
+<link rel="stylesheet" href="css/bootstrap.min.css">
+<link rel="stylesheet" href="css/all.min.css">
+<link rel="stylesheet" type="text/css" href="css/style.css">
+<script src="js/bootstrap.min.js"></script>
+<script src="js/popper.min.js"></script>
+<script src="js/jquery-3.6.0.min.js"></script>
+<script src="js/all.min.js"></script>
